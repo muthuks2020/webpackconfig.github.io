8c1faf6a9f0213667995f1de8b9d260b1dd91e11
Muthu <muthukumaraswamy.b@2adpro.com>
2018-05-16 10:54:14 +0530

    updates

---
+++ index.js
@@ -0,0 +1,182 @@
+import React from 'react';
+import { render } from 'react-dom';
+import jsStringify from "javascript-stringify";
+import _ from "lodash";
+import { features, createWebpackConfig, createBabelConfig, getNpmDependencies } from "./src/configurator";
+
+import prism from "prismjs";
+
+const Feature = ({feature, selected, setSelected}) => (
+    <label key={feature} className="feature-container">
+            {feature}
+            <input
+                checked={selected}
+                onClick={() => setSelected(feature)}
+                type="checkbox" />
+                <span className="checkmark"></span>
+    </label>
+);
+
+class Features extends React.Component {
+    render() {
+        const { features, selected, setSelected } = this.props;
+        const groupedFeatures = _.chain(features)
+              .mapValues((v,k,o) => Object.assign({}, v, { feature: k }))
+              .groupBy("group")
+              .value();
+
+        return (
+            <div className="features">
+                {_.map(groupedFeatures, (featureList, group) => (
+                    <div className="feature-group">
+                        <div className="feature-group-name">
+                            {group !== "undefined" ? group : ""}
+                        </div>
+                        <div className="feature-group-container">
+                        {_.map(featureList, (feature) => (
+                            <Feature
+                                feature={feature.feature}
+                                selected={selected[feature.feature]}
+                                setSelected={setSelected}
+                            />
+                        ))}
+                    </div>
+                    </div>
+                ))}
+
+            </div>
+        );
+    }
+}
+
+const logFeatureClickToGa = (feature, selected) => {
+    const eventAction = selected ? 'select' : 'deselect';
+    window.gtag("event", eventAction, {
+        "event_category": "features",
+        "event_label": feature
+    })
+}
+
+const Header = ({selected, setSelected}) => (
+    <div className="header">
+        <h1>webpack 4 configurator</h1>
+        <h2>Create a <i>personalized</i> and <i>optimized</i> webpack.config.js!</h2>
+
+        <div >
+            <div className="start-here">Start here! What features do you need?</div>
+        </div>
+        <Features
+            features={features}
+            selected={selected}
+            setSelected={setSelected}/>
+    </div>
+);
+
+const Footer = () => (
+    <div className="footer">
+        <h3>What is this?  </h3>
+        <p>
+            When using this tool, you get a webpack.config.js that is created just for <i>you</i>. It's a great starting point for further development. The webpack.config.js will create an optimized bundle based on <a href="http://blog.jakoblind.no/3-ways-to-reduce-webpack-bundle-size/">best practices</a>. Advanced optimizations such as code splitting is not (yet?) supported with this tool.
+        </p>
+        <h4>Want more?</h4>
+        <p>Want to be notified when I build more cool stuff like this? And also get early access to articles?</p>
+        <a className="myButton" href="https://www.getdrip.com/forms/81916834/submissions/new">Sign up to my newsletter</a>
+        <h4>Found a bug or want a feature?</h4>
+        <p>Contact me on <a href="https://twitter.com/karljakoblind">twitter</a> or file an issue or even better make a PR on the <a href="https://github.com/jakoblind/webpack-configurator">github repo</a>. Yes this is open source.</p>
+        <h4>Who made this thing?</h4>
+        <p>
+            It's me, Jakob who made this. I write <a href="http://blog.jakoblind.no">a blog about React</a> that you might want to check out if you liked this! I am also on <a href="https://twitter.com/karljakoblind">twitter</a>.
+        </p>
+
+    </div>
+);
+
+const WebpackConfigArea = ({newWebpackConfig}) => {
+    const highlightedWebpackConfig = () => {
+        return {
+            __html: Prism.highlight(newWebpackConfig, Prism.languages.javascript, 'javascript')
+        };
+    };
+
+    return (
+        <div className="left-section">
+            <h3>Your personal webpack.config.js</h3>
+            <pre><code className="language-css" dangerouslySetInnerHTML={highlightedWebpackConfig()}></code></pre>
+        </div>
+    )
+}
+
+const StepByStepArea = ({newNpmConfig, newBabelConfig, isReact}) => {
+    const npmInstallCommand = _.isEmpty(newNpmConfig.dependencies) ? "" : "\nnpm install " + newNpmConfig.dependencies.join(" ")
+    const npmCommand = "mkdir myapp\ncd myapp\nnpm init -y\nnpm install --save-dev " + newNpmConfig.devDependencies.join(" ") + npmInstallCommand
+
+    return (
+        <div className="right-section">
+            <h3>Create your project in {newBabelConfig ? 4 : 3} easy steps!</h3>
+            <ol>
+                <li>Create an NPM project and install dependencies</li>
+                <textarea readOnly={true} rows="6" cols="50" value={npmCommand}/>
+
+                <li>Create <i>webpack.config.js</i> in the root and copy the contents of the generated file</li>
+
+                {newBabelConfig ? <div><li>Create <i>.babelrc</i> in the root and copy the contents from below</li>
+                    <textarea readOnly={true} rows="11" cols="50" value={newBabelConfig}/></div> : null}
+
+                {isReact ? <li>Create folders src and dist and create your <a href="https://s3-eu-west-1.amazonaws.com/jakoblind/react/index.js">index.js</a> file in src folder and <a href="https://s3-eu-west-1.amazonaws.com/jakoblind/react/index.html">index.html</a> in the dist folder</li> : <li>Create folders src and dist and create your index.js file in src folder</li>}
+            </ol>
+            <a href="http://blog.jakoblind.no/react-with-webpack-babel-npm/">Need more detailed instructions?</a>
+        </div>
+
+    )
+}
+
+class Configurator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { selected: { } }
+        this.setSelected = this.setSelected.bind(this);
+    }
+    setSelected(feature) {
+        const setToSelected = !this.state.selected[feature]
+        logFeatureClickToGa(feature, setToSelected);
+        const selected = Object.assign({}, this.state.selected, { [feature]: setToSelected });
+
+        // only possible to select one of Vue or React. Needing both is an edge case
+        // that is probably very rare. It adds much complexity to support both.
+        if (feature === "Vue" && setToSelected) {
+            selected["React"] = !setToSelected;
+        } else if (feature === "React" && setToSelected) {
+            selected["Vue"] = !setToSelected;
+        }
+        this.setState({ selected });
+    }
+    selectedArray(){
+        return _.chain(this.state.selected).map((v, k) => v ? k : null).reject(_.isNull).value();
+    }
+    render() {
+        const newWebpackConfig = createWebpackConfig(this.selectedArray());
+        const newBabelConfig = createBabelConfig(this.selectedArray());
+        const newNpmConfig = getNpmDependencies(this.selectedArray());
+
+        return (
+            <div>
+                <Header
+                    selected={this.state.selected}
+                    setSelected={this.setSelected} />
+                <div className="container">
+                    <WebpackConfigArea newWebpackConfig={newWebpackConfig}/>
+                    <StepByStepArea
+                        newNpmConfig={newNpmConfig}
+                        newBabelConfig={newBabelConfig}
+                        isReact={_.includes(this.selectedArray(), "React")}/>
+                </div>
+                <Footer/>
+            </div>)
+    }
+}
+
+const App = () => (
+    <Configurator />
+);
+
+render(<App />, document.getElementById('app'));
---
+++ src/configurator.js
@@ -0,0 +1,234 @@
+import { baseWebpack, baseWebpackImports } from "./templates";
+
+const jsStringify = require("javascript-stringify");
+const _ = require("lodash");
+
+function addPlugin(webpackConfig, plugin) {
+    if (!webpackConfig.plugins) {
+        return Object.assign({}, webpackConfig, {
+            plugins: [plugin]
+        })
+    }
+    return Object.assign({}, webpackConfig, {
+        plugins: _.concat(webpackConfig.plugins, plugin)
+    })
+}
+
+function addModuleRule(webpackConfig, ruleOrRules) {
+    const isManyRules = _.isArray(ruleOrRules);
+    const rules = isManyRules ? ruleOrRules : [ruleOrRules];
+
+    if (!_.has(webpackConfig, "module.rules")) {
+        return Object.assign({}, webpackConfig, {
+            module: {
+                rules
+            }
+        })
+    }
+
+    const newWebpackConfig = _.cloneDeep(webpackConfig);
+    newWebpackConfig.module.rules = _.union(newWebpackConfig.module.rules, rules)
+
+    return newWebpackConfig;
+}
+
+const getStyleLoaderOrVueStyleLoader = (configItems) => _.includes(configItems, "Vue") ? 'vue-style-loader' : 'style-loader';
+
+const getStyleLoaderDependencyIfNeeded = (configItems) => _.includes(configItems, "Vue") ? [] : ['style-loader'];
+
+export const features = (() => {
+    const features = {
+        "React": {
+            group: "Main library",
+            babel: (babelConfig) => Object.assign({}, babelConfig, {
+                "presets": [['env', { modules: false }], "react"]
+            }),
+            dependencies: (configItems) => ["react", "react-dom"],
+            devDependencies: (configItems) => ["babel-loader", "babel-preset-react", "babel-core", "babel-preset-env"],
+            webpack: (webpackConfig) =>
+                Object.assign({}, webpackConfig, addModuleRule(webpackConfig, {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: 'babel-loader'
+                }), {
+                    resolve: {
+                        extensions: ['.js', '.jsx']
+                    }
+                })
+        },
+        "Vue": {
+            group: "Main library",
+            webpackImports: ["const VueLoaderPlugin = require('vue-loader/lib/plugin');"],
+            webpack: (webpackConfig) => {
+                const webpackConfigWithRule = addModuleRule(webpackConfig,  [{
+                    test: /\.vue$/,
+                    loader: 'vue-loader'
+                }, {
+                    test: /\.js$/,
+                    loader: 'babel-loader'
+                }])
+
+                return addPlugin(webpackConfigWithRule, "CODE:new VueLoaderPlugin()");
+            },
+            babel: (babelConfig) => Object.assign({}, babelConfig, {
+                "presets": [['env', { modules: false }]]
+            }),
+            devDependencies: (configItems) => ["vue-loader", "vue-template-compiler", "babel-loader", "babel-core", "babel-preset-env"],
+            dependencies: (configItems) => ["vue"]
+        },
+        "CSS": {
+            group: "Styling",
+            devDependencies: (configItems) => _.concat(["css-loader"], getStyleLoaderDependencyIfNeeded(configItems)),
+            webpack: (webpackConfig, configItems) => addModuleRule(webpackConfig, {
+                test: /\.css$/,
+                use: [
+                    getStyleLoaderOrVueStyleLoader(configItems),
+                    'css-loader'
+                ]
+            })
+        },
+        "Sass": {
+            group: "Styling",
+            devDependencies: (configItems) => _.concat(["css-loader", "sass-loader", "node-sass"], getStyleLoaderDependencyIfNeeded(configItems)),
+            webpack: (webpackConfig, configItems) => addModuleRule(webpackConfig, {
+                test: /\.scss$/,
+                use: [
+                    getStyleLoaderOrVueStyleLoader(configItems),
+                    'css-loader',
+                    'sass-loader'
+                ]
+            })
+        },
+        "Less": {
+            group: "Styling",
+            devDependencies: (configItems) => _.concat(["css-loader", "less-loader"], getStyleLoaderDependencyIfNeeded(configItems)),
+            webpack: (webpackConfig, configItems) => addModuleRule(webpackConfig, {
+                test: /\.less$/,
+                use: [
+                    getStyleLoaderOrVueStyleLoader(configItems),
+                    'css-loader',
+                    'less-loader'
+                ]
+            })
+        },
+        "stylus": {
+            group: "Styling",
+            devDependencies: (configItems) => _.concat(["css-loader", "stylus-loader"], getStyleLoaderDependencyIfNeeded(configItems)),
+            webpack: (webpackConfig, configItems) => addModuleRule(webpackConfig, {
+                test: /\.styl$/,
+                use: [
+                    getStyleLoaderOrVueStyleLoader(configItems),
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            })
+        },
+        "SVG": {
+            group: "Image",
+            devDependencies: (configItems) => ["file-loader"],
+            webpack: (webpackConfig) => addModuleRule(webpackConfig, {
+                test: /\.svg$/,
+                use: [
+                    "file-loader"
+                ]
+            })
+        },
+        "PNG": {
+            group: "Image",
+            devDependencies: (configItems) => ["url-loader"],
+            webpack: (webpackConfig) => addModuleRule(webpackConfig, {
+                test: /\.png$/,
+                use: [{
+                    loader: 'url-loader',
+                    options:{
+                        mimetype:'image/png'
+                    }
+                }]
+            })
+        },
+        "moment": {
+            group: "Utilities",
+            "dependencies": (configItems) => ["moment"],
+            webpack: (webpackConfig) => addPlugin(webpackConfig, "CODE:new webpack.ContextReplacementPlugin(/moment[\\\/\\\\]locale$/, /en/)")
+        },
+        "lodash": {
+            group: "Utilities",
+            babel: _.identity,
+            dependencies: (configItems) => ["lodash"],
+            devDependencies: (configItems) => ["lodash-webpack-plugin"],
+            webpackImports: ["const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');"],
+            webpack:
+            (webpackConfig) => addPlugin(webpackConfig, "CODE:new LodashModuleReplacementPlugin")
+        },
+        "Production mode": {
+            webpack: (webpackConfig) => Object.assign({}, webpackConfig, {"mode": "production"})
+        }
+    }
+    return _.mapValues(features, (item) => {
+        if (!item.babel) {
+            item.babel = _.identity;
+        }
+        if (!item.webpack) {
+            item.webpack = _.identity;
+        }
+        if (!item.webpackImports) {
+            item.webpackImports = [];
+        }
+        if (!item.dependencies) {
+            item.dependencies = () => [];
+        }
+        if (!item.devDependencies) {
+            item.devDependencies = () => [];
+        }
+        return item;
+    })
+})()
+
+function stringifyReplacer (value, indent, stringify) {
+  if (typeof value === 'string' && value.startsWith("CODE:")) {
+      return value.replace(/"/g, '\\"').replace(/^CODE:/, "");
+  }
+
+  return stringify(value);
+}
+
+function createConfig(configItems, configType) {
+    const base = configType === "webpack" ? baseWebpack : {};
+    return jsStringify(_.reduce(configItems, (acc, currentValue) => (features[currentValue][configType](acc, configItems)), base), stringifyReplacer, 2)
+}
+
+export function getNpmDependencies(configItems) {
+    const dependencies = _.chain(configItems)
+          .reduce((acc, currentValue) => (_.concat(acc, features[currentValue]["dependencies"](configItems))), [])
+          .uniq()
+          .value();
+
+    const devDependencies = _.chain(configItems)
+          .reduce((acc, currentValue) => (_.concat(acc, features[currentValue]["devDependencies"](configItems))), ["webpack"])
+          .uniq()
+          .value();
+
+    return {
+        dependencies,
+        devDependencies
+    }
+}
+
+export function getWebpackImports(configItems) {
+    return _.reduce(configItems, (acc, currentValue) => (_.concat(acc, features[currentValue]["webpackImports"])), [])
+}
+
+export function createBabelConfig(configItems) {
+    const config = createConfig(configItems, "babel");
+    return config === "{}" ? null : config;
+
+}
+
+export function createWebpackConfig(configItems) {
+    const imports = _.concat(baseWebpackImports, getWebpackImports(configItems))
+    return `${imports.join("\n")}
+
+const config = ${createConfig(configItems, "webpack")}
+
+module.exports = config;`;
+}
